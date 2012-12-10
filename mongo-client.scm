ad1d02f8a827925c02d3d0462dede6b1f8694134
Thomas Hintz <t@thinz.com>
2012-12-10 13:14:12 +0000

    Convert to medea.

--- mongo-client.scm
+++ mongo-client.scm
@@ -2,8 +2,6 @@
 (use http-client medea intarweb uri-common)
 
 ;;;;;;;;;;;;;;;; utilities ;;;;;;;;;;;;;;;;;;;;
-(define (->json-string json-struct)
-  (with-output-to-string (lambda () (write-json json-struct))))
 
 ;;;;;;;;;;;;;;; parameters ;;;;;;;;;;;;;;;;;;;;
 (define endpoint (make-parameter "http://localhost:27080"))
@@ -12,7 +10,7 @@
 (define safe (make-parameter #f))
 
 ;;;;;;;;;;;;;;;;;; api ;;;;;;;;;;;;;;;;;;;;;;;
-(define (mongo-req #!key (connection-name (connection-name))
+(define (mdb-req #!key (connection-name (connection-name))
                    (endpoint (endpoint)) parameters path (method 'GET)
                    (action ""))
   (with-input-from-request
@@ -28,47 +26,50 @@
                parameters)
        #f)
    read-json))
-(mongo-req action: "_hello")
-(define (connect #!key server connection-name)
+
+(define (mdb-hello)
+  (mdb-req action: "_hello"))
+
+(define (mdb-connect #!key server connection-name)
   (parameterize
    ((database #f))
-   (mongo-req path: #f connection-name: connection-name
+   (mdb-req path: #f connection-name: connection-name
               method: 'POST action: "_connect"
               parameters: (if server `((server . ,server)) '((nothing . nothing))))))
 
-(define-syntax with-database
+(define-syntax with-mdb-database
   (syntax-rules ()
     ((_ the-database body ...)
      (parameterize ((database the-database)) body ...))))
 
-(define (insert resource document)
-  (mongo-req
+(define (mdb-insert resource document)
+  (mdb-req
    path: resource action: "_insert" method: 'POST
    parameters:
-   `((docs . ,(->json-string document)))))
+   `((docs . ,(json->string document)))))
 
-(define (update resource document criteria)
-  (mongo-req
+(define (mdb-update resource document criteria)
+  (mdb-req
    path: resource action: "_update" method: 'POST
    parameters:
-   `((newobj . ,(->json-string document)) (criteria . ,(->json-string criteria)))))
+   `((newobj . ,(json->string document)) (criteria . ,(json->string criteria)))))
 
-(define (remove resource criteria)
-  (mongo-req
+(define (mdb-remove resource criteria)
+  (mdb-req
    path: resource action: "_remove" method: 'POST
    parameters:
-   `((criteria . ,(->json-string criteria)))))
+   `((criteria . ,(json->string criteria)))))
 
-(define (mongo-find resource #!key criteria fields sort skip limit explain batch-size)
-  (mongo-req
+(define (mdb-find resource #!key criteria fields sort skip limit explain batch-size)
+  (mdb-req
    action: (string-append "_find?criteria="
-                          (uri-encode-string (->json-string criteria)))
+                          (uri-encode-string (json->string criteria)))
    method: 'GET path: resource))
 
-(define (mongo-cmd resource cmd)
-  (mongo-req
+(define (mdb-cmd resource cmd)
+  (mdb-req
    path: resource method: 'POST action: "_cmd"
-   parameters: `((cmd . ,(->json-string cmd)))))
+   parameters: `((cmd . ,(json->string cmd)))))
 
 ;;; for testing
 ;(database "clienttest")
--- tests.scm
+++ tests.scm
@@ -1,24 +1,25 @@
 (use test)
 (load "mongo-client.scm")
 
+(define (contains-oid json-result)
+  (if (alist-ref 'oids json-result) #t #f))
+
 (test-begin "all")
 
 (test-group
- "->json-string"
- (test "{\"a\":\"b\"}" (->json-string '((a . "b"))))
- (test "{\"a\":{\"b\":\"c\"}}" (->json-string '((a . ((b . "c"))))))
- (test "{\"a\":[\"b\",\"c\"]}" (->json-string '((a . #("b" "c"))))))
+ "connect"
+ (test '((ok . 1) (server . "http://localhost:27017") (name . "default"))
+       (mdb-connect server: "http://localhost:27017")))
 
 (test-group
- "connect"
- (test '#(("ok" . 1) ("server" . "http://localhost:27017") ("name" . "default"))
-       (connect server: "http://localhost:27017")))
+ "hello"
+ (test '((ok . 1) (msg . "Uh, we had a slight weapons malfunction, but uh... everything's perfectly all right now. We're fine. We're all fine here now, thank you. How are you?"))
+       (mdb-hello)))
 
-;; (test-group
-;;  "insert"
-;;  (connect server: "http://localhost:27017")
-;;  (database "testdatabase")
-;;  (test '#(("oids" . #(("$oid" . "50bf4e8e4c860b61384fa132"))))
-;;        (insert "test" '#((name . "stuff")))))
+(test-group
+ "insert"
+ (mdb-connect server: "http://localhost:27017")
+ (database "testdatabase")
+ (test #t (contains-oid (mdb-insert "test" '((name . "stuff"))))))
 
 (test-end "all")
